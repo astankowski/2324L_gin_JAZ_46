f5aa617aebde00b41160988f319fa9e43119a39d
s27549 <s27549@pjwstk.edu.pl>
2024-04-21 12:41:29 +0200

    h2 working, no data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO PRs (weight, exercise, owner_name) VALUES (100, 'BENCH_PRESS', 'Jan');
+INSERT INTO PRs (weight, exercise, owner_name) VALUES (140, 'SQUAT', 'Kuba');
+INSERT INTO PRs (weight, exercise, owner_name) VALUES (180, 'DEADLIFT', 'Daniel');
+INSERT INTO PRs (weight, exercise, owner_name) VALUES (120, 'BARBELL_ROW', 'Jan');
+INSERT INTO PRs (weight, exercise, owner_name) VALUES (60, 'OVERHEAD_PRESS', 'Marek');
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table PRs(
+                    ID int not null AUTO_INCREMENT,
+                    WEIGHT FLOAT,
+                    EXERCISE varchar(100),
+                    OWNER_NAME varchar(100),
+                    PRIMARY KEY ( ID )
+);
